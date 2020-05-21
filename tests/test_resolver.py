acde703c68b453ebbc93014ea46dbc115d5062a0
Julian Stier <mail@julian-stier.de>
2020-05-21 11:53:42 +0200

    Added a version to the package and started taking ideas from jsonschema to build an independent resolver mechanism

---
+++ tests/test_resolver.py
@@ -0,0 +1,54 @@
+from jsbuilder.builder import DefaultJsonSchemaResolver, JsonSchemaString, JsonSchemaNumber, JsonSchemaArray, \
+    JsonSchemaInteger, JsonSchemaObject
+
+
+def test_dev():
+    schema = {
+        "$id": "",
+        "definitions": {
+            "my_arr": {
+                "type": "array",
+                "minItems": 1,
+                "items": {"$ref": "#"}
+            }
+        }
+    }
+    resolver = DefaultJsonSchemaResolver.from_schema(schema)
+
+    with resolver.in_scope('schemaArray'):
+        resolver.resolve('a')
+
+
+def test_default_string_resolves_to_stringnode():
+    resolver = DefaultJsonSchemaResolver.get_instance()
+    resolved_node = resolver.resolve('str')
+
+    assert resolved_node == JsonSchemaString()
+
+
+def test_default_float_resolves_to_numbernode():
+    resolver = DefaultJsonSchemaResolver.get_instance()
+    resolved_node = resolver.resolve(5.0)
+
+    assert resolved_node == JsonSchemaNumber()
+
+
+def test_default_int_resolves_to_integernode():
+    resolver = DefaultJsonSchemaResolver.get_instance()
+    resolved_node = resolver.resolve(5)
+
+    assert resolved_node == JsonSchemaInteger()
+
+
+def test_default_array_resolves_to_arraynode():
+    resolver = DefaultJsonSchemaResolver.get_instance()
+    resolved_node = resolver.resolve([])
+
+    assert resolved_node == JsonSchemaArray()
+
+
+def test_default_dict_resolves_to_objectnode():
+    resolver = DefaultJsonSchemaResolver.get_instance()
+    resolved_node = resolver.resolve({})
+
+    assert resolved_node == JsonSchemaObject()
